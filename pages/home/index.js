0c18e939e95bcff9d49e6859a105abf454ab379e
Plinio <plinio.duartes@hotmail.com>
2020-06-16 11:11:30 -0300

    initial commit

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import withAnalytics from '~/hocs/withAnalytics';
 import Wrapper from './styles';
 import { Link } from '../../routes';
 
 const HomePage = () => {
-  return (
-    <Wrapper>
-        <h1>
-            HomePage
-        </h1>
-        <div className="buttons">
-            <Link route="/sobre-a-empresa">
-                <a>Sobre a empresa</a>
-            </Link>
-            <Link route="/contato">
-                <a>Contato</a>    
-            </Link>
-        </div>        
-    </Wrapper>
-  );
+
+    useEffect(() => {
+        fetch(`http://localhost/react-wordpress/wp-json/wp/v2/posts/5`)
+        .then((respose) => respose.json())
+        .then((resposeJson) => {
+            console.log(resposeJson);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    })
+
+    return (
+        <Wrapper>
+            <h1>
+                HomePage
+            </h1>
+            <div className="buttons">
+                <Link route="/sobre-a-empresa">
+                    <a>Sobre a empresa</a>
+                </Link>
+                <Link route="/contato">
+                    <a>Contato</a>    
+                </Link>
+            </div>        
+        </Wrapper>
+    );
 }
 
 
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const { createServer } = require('http');
 const next = require('next');
 const routes = require('./routes');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
+const express = require('express')
+
+const sitemapOptions = {
+    root: __dirname + '/static/sitemap/',
+    headers: {
+        'Content-Type': 'text/xml;charset=UTF-8'
+    }
+};
 
 app.prepare().then(() => {
-    createServer(handler, res => {
+    const server = express();
+    server.get('/sitemap.xml', (req, res) => res.status(200).sendFile('sitemap.xml', sitemapOptions));
+    server.use('/wp-admin', createProxyMiddleware({
+        target: 'http://localhost/react-wordpress',
+        changeOrigin: true,
+    }));
+    server.use(handler, res => {
         res.setHeader("Cache-Control", "public, max-age=365d, immutable");
-    }).listen(3000);
+    }).listen(8090);
 });
